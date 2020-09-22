535e3b3154ccb1b42eb671c0e7c5cf2cb6096adb
yuriruf <yurithedeveloper@gmail.com>
2020-09-22 15:55:53 -0300

    Created the project and Created Mapcreator and Tile (Unity 2020.1)

---
+++ Project/Lawn Mower/Assets/Script/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+	public Vector2 position = new Vector2(0,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
